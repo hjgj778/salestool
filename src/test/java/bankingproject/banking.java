7e64c0db1fba90be94ccc851be8aed5b76b99335
venkatapardhasaradhi <venkatapardhasaradhi@rithvik-pc>
2017-09-01 13:17:09 +1000

    test demo

---
+++ src/test/java/bankingproject/banking.java
@@ -0,0 +1,13 @@
+package bankingproject;
+
+import org.testng.annotations.Test;
+
+public class banking {
+	
+	@Test
+	public void testhello()
+	{
+		System.out.println("Welcome to maven world");
+	}
+
+}
